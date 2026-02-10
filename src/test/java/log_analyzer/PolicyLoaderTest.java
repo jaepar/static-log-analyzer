72fd3817fab75b9e0da0d471cbb3be561ab16cb7
hyun7931 <shlee4185@gmail.com>
2026-02-10 17:56:32 +0900

    feat: add jacoco, fix PolicyLoaderTest

--- src/test/java/log_analyzer/PolicyLoaderTest.java
+++ src/test/java/log_analyzer/PolicyLoaderTest.java
@@ -10,6 +10,8 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import log_analyzer.exception.ParserException;
 import log_analyzer.policy.entity.ForbiddenFieldRule;
@@ -18,8 +20,9 @@ import log_analyzer.policy.service.LoggingPolicy;
 import log_analyzer.policy.service.PolicyLoader;
 
 class PolicyLoaderTest {
+	// load
     @Test
-    @DisplayName("유효한 YAML 파일을 로드하면 LoggingPolicy 객체가 정상적으로 생성")
+    @DisplayName("유효한 YAML 파일을 로드하면 LoggingPolicy 객체가 정상적으로 생성된다")
     void test1(@TempDir Path tempDir) throws IOException {
         // Given
         String yamlContent = """
@@ -46,12 +49,13 @@ class PolicyLoaderTest {
     }
     
     @Test
-    @DisplayName("forbiddenFields가 리스트가 아닌 경우 ParserException")
+    @DisplayName("forbiddenFields가 리스트가 아닌 경우 ParserException - forbiddenFields must be a list 에러가 발생한다")
     void test2(@TempDir Path tempDir) throws IOException {
         String yamlContent = """
             forbiddenFields: password
             logMethods:
               - log.info
+              - log.error
             """;
         Path policyFile = tempDir.resolve("policy.yml");
         Files.writeString(policyFile, yamlContent);
@@ -64,7 +68,7 @@ class PolicyLoaderTest {
     }
     
     @Test
-    @DisplayName("forbiddenFields의 match 필드가 없으면 기본값은 exact로")
+    @DisplayName("forbiddenFields의 match 필드가 없으면 기본값은 exact로 실행된다")
     void test3(@TempDir Path tempDir) throws IOException {
         String yamlContent = """
             forbiddenFields:
@@ -84,7 +88,7 @@ class PolicyLoaderTest {
     }
     
     @Test
-    @DisplayName("root가 Map이 아닌 경우")
+    @DisplayName("root가 Map이 아닌 경우 ParserException - root is not a map에러가 발생한다")
     void test4(@TempDir Path tempDir) throws IOException {
         String yamlContent = """
                 - item1
@@ -99,4 +103,27 @@ class PolicyLoaderTest {
             });
             assertTrue(exception.getMessage().contains("root is not a map"));
         }
+    
+    // validateFileExtension
+    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"policy.yml", "policy.yaml"})
+    @DisplayName("확장자가 yml 또는 yaml이면 예외가 발생하지 않는다")
+    void test5(String fileName) {
+        PolicyLoader loader = new PolicyLoader();
+        assertDoesNotThrow(() -> loader.validateFileExtension(fileName));
+    }
+
+    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"policy.txt", "policy.json", "policy.xml"})
+    @DisplayName("확장자가 yml/yaml이 아니면 ParserException - 유효하지 않은 파일 확장자")
+    void test6(String fileName) {
+        PolicyLoader loader = new PolicyLoader();
+        ParserException e = assertThrows(ParserException.class,
+            () -> loader.validateFileExtension(fileName));
+
+        assertTrue(e.getMessage().contains("유효하지 않은 파일 확장자"));
+    }
+
 }
